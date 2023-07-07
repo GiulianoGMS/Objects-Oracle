92bf083325744b047a6019ae9fb3209ff33342cd
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-07-07 16:15:26 -0300

    Update NAGF_BUSCAULTDTAPEDIDO.sql

--- NAGF_BUSCAULTDTAPEDIDO.sql
+++ NAGF_BUSCAULTDTAPEDIDO.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE FUNCTION CONSINCO.NAGF_BUSCAULTDTAPEDIDO (p_SeqLoteModelo NUMBER) RETURN DATE IS
-                                                                           v_proxped    DATE;
+  /* Criado por Giuliano | 07/07/2023 */                                   v_proxped    DATE;
                                                                            v_diasconfig NUMBER;
                                                                            v_diasemana  VARCHAR2(10);
                                                                            v_dia        VARCHAR2(10);
                                                                            v_dta        DATE;
                                                                            v_tp         VARCHAR2(1);
  BEGIN
-  -- Valida Parametrizacoes na NAGT_ABASTECIMENTOCONFIG
+  -- Valida Parametrizacoes na NAGT_CONTROLELOTECOMPRA
   -- Tradur o dia configurado pra utilizar no Next_Day
   
   SELECT DIASCONFIG, DECODE(UPPER(DIASEMANA),  'SEGUNDA' , 'MONDAY',
@@ -14,13 +14,13 @@ CREATE OR REPLACE FUNCTION CONSINCO.NAGF_BUSCAULTDTAPEDIDO (p_SeqLoteModelo NUMB
                                                'QUARTA'  , 'WEDNESDAY',
                                                'QUINTA'  , 'THURSDAY',
                                                'SEXTA'   , 'FRIDAY',
-                                               'SABADO ' , 'SATURDAY',
+                                               'SABADO'  , 'SATURDAY',
                                                'DOMINGO' , 'SUNDAY')
   --
     INTO v_diasconfig, v_diasemana
     FROM CONSINCO.NAGT_CONTROLELOTECOMPRA X
    WHERE X.SEQLOTEMODELO = p_SeqLoteModelo;
-   
+  --
   -- Primeiro resultado para validar se o dia semana é igual ao dia atual
   -- GREATEST pega a maior data entre Inclusão ou Fechamento
   SELECT TRIM(TO_CHAR(TRUNC(GREATEST(MAX(DTAHORINCLUSAO), NVL(MAX(DTAHORFECHAMENTO),MAX(DTAHORINCLUSAO))) + v_diasconfig),'DAY')), 
@@ -35,42 +35,53 @@ CREATE OR REPLACE FUNCTION CONSINCO.NAGF_BUSCAULTDTAPEDIDO (p_SeqLoteModelo NUMB
   --
     FROM CONSINCO.MAC_GERCOMPRA A
    WHERE 1=1
-     AND A.SEQGERMODELOCOMPRA = p_SeqLoteModelo 
-     OR A.SEQGERCOMPRA = p_SeqLoteModelo AND TIPOLOTE = 'M';
+     AND A.SEQGERMODELOCOMPRA = p_SeqLoteModelo AND USUINCLUSAO = 'JOBGERALOTE'
+      OR A.SEQGERCOMPRA = p_SeqLoteModelo AND TIPOLOTE = 'M';
   --
   -- Tratamento para Lote Modelo, valida apenas dia da semana programado e retorna hoje
-  IF v_tp = 'M'
- AND v_diasemana = TRIM(TO_CHAR(SYSDATE, 'DAY'))
-  OR
+       IF 
+             v_tp = 'M'
+         AND v_diasemana = TRIM(TO_CHAR(SYSDATE, 'DAY'))
+          OR
   --
   -- Para lotes normais | Se o dia retorno do calculo for igual hoje, retorna hoje  
-      v_diasemana = v_dia 
-  AND v_dta = TRUNC(SYSDATE)
-  AND v_tp != 'M' -- Tira Modelo
-  
-     THEN
-     SELECT TRUNC(SYSDATE)
-       INTO v_proxped
-       FROM DUAL;
+             v_diasemana = v_dia 
+         AND v_dta = TRUNC(SYSDATE)
+         AND v_tp != 'M' -- Tira Modelo
+          
+        THEN
+      SELECT TRUNC(SYSDATE)
+        INTO v_proxped
+        FROM DUAL;
+  --
+  -- Busca a próxima data valida para lotes modelos que não possuem o dia atual = diasemana config
+   ELSIF
+             v_tp = 'M'
+         AND v_diasemana != TRIM(TO_CHAR(SYSDATE, 'DAY'))
+         
+        THEN
+      SELECT NEXT_DAY(SYSDATE, v_diasemana)
+        INTO v_proxped
+        FROM DUAL;
   --
   -- Caso contrário, próximo dia da semana que está parametrizado
    ELSE
 
-  SELECT NEXT_DAY((SELECT GREATEST(MAX(DTAHORINCLUSAO), NVL(MAX(DTAHORFECHAMENTO),MAX(DTAHORINCLUSAO))) -1
-    FROM CONSINCO.MAC_GERCOMPRA A 
-   WHERE 1=1
-     AND A.SEQGERMODELOCOMPRA = p_SeqLoteModelo 
-      OR A.SEQGERCOMPRA = p_SeqLoteModelo AND TIPOLOTE = 'M')
-   + v_diasconfig, v_diasemana)
-   INTO v_proxped
-   FROM DUAL;
+      SELECT NEXT_DAY((SELECT GREATEST(MAX(DTAHORINCLUSAO), NVL(MAX(DTAHORFECHAMENTO),MAX(DTAHORINCLUSAO))) -1
+        FROM CONSINCO.MAC_GERCOMPRA A 
+       WHERE 1=1
+         AND A.SEQGERMODELOCOMPRA = p_SeqLoteModelo 
+          OR A.SEQGERCOMPRA = p_SeqLoteModelo AND TIPOLOTE = 'M')
+           + v_diasconfig, v_diasemana)
+        INTO v_proxped
+        FROM DUAL;
 
-    END IF;
+   END IF;
 
  RETURN v_proxped;
  EXCEPTION
    WHEN NO_DATA_FOUND THEN
-     RETURN TRUNC(SYSDATE) -1;
+     RETURN NULL; --TRUNC(SYSDATE) + 100;
      WHEN OTHERS THEN
      DBMS_OUTPUT.PUT_LINE(p_SeqLoteModelo);
 END;
