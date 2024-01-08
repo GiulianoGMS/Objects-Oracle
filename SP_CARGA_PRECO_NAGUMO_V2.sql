5ede10de829bcb0a9b124e80cd47944b6683a190
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2024-01-08 13:31:42 -0300

    Update SP_CARGA_PRECO_NAGUMO_V2.sql

--- SP_CARGA_PRECO_NAGUMO_V2.sql
+++ SP_CARGA_PRECO_NAGUMO_V2.sql
@@ -1,109 +1,113 @@
-create or replace procedure sp_carga_preco_nagumo_v2(pnNroJob     in   number,
-                                                     pnModEmpresa in   number   ) is
-  vsSoft           varchar2(50);
-  vsHora           varchar2(2);
-  vsTipoCarga      varchar2(10);
-  obj_param_smtp   c5_tp_param_smtp;
-  vsSql            varchar2(2000);
-begin
-  select to_char(sysdate, 'HH24')
-  into   vsHora
-  from   dual;
-  if vsHora in ('01', '09', '13')  then
-     vsSql :=
+CREATE OR REPLACE PROCEDURE SP_CARGA_PRECO_NAGUMO_V2(PNNROJOB     IN   NUMBER,
+                                                     PNMODEMPRESA IN   NUMBER   ) IS
+  VSSOFT           VARCHAR2(50);
+  VSHORA           VARCHAR2(2);
+  VSTIPOCARGA      VARCHAR2(10);
+  OBJ_PARAM_SMTP   C5_TP_PARAM_SMTP;
+  VSSQL            VARCHAR2(2000);
+BEGIN
+  SELECT TO_CHAR(SYSDATE, 'HH24')
+  INTO   VSHORA
+  FROM   DUAL;
+  IF VSHORA IN ('01', '09', '13')  THEN
+     VSSQL :=
      'insert into nag_job(nrojob, dtamovimento,    erro)
-                 values ('||pnNroJob||',    trunc(sysdate), ''Inicio: ' || to_char(sysdate,'dd-mm-yyyy HH24:mi:ss')||''')';
-     execute immediate vsSql;
-  end if;
-  if vsHora = '01' then
-     vsTipoCarga := 'Total';
-  elsif vsHora in ('09', '13')  then
-        vsTipoCarga := 'Parcial';
-  end if;
-  if vsHora in ('01', '09', '13')  then
-     for i in (select * from max_empresa a
-               where a.status = 'A'
-               and  mod(a.nroempresa,5) = pnModEmpresa
-               and  nroempresa < 500
-               order by 1)
-     loop
-       obj_param_smtp := c5_tp_param_smtp(1);
-       if vsHora in ('01', '09', '13')  then
-          if vsHora <> '03' then
-             --gerando promocao
-             pkg_mad_admpreco.SP_GeraPromocao(i.nrosegmentoprinc, i.nroempresa, trunc(sysdate), 'AUTOMATICO');
-             --validando precos
-             pkg_mad_admpreco.SP_ValidaPreco(i.nrosegmentoprinc, i.nroempresa,'AUTOMATICO', 'T');
-          end if;
-          begin
-            spmrl_cargapdvcoral(i.nroempresa, trunc(sysdate), vsTipoCarga, null);
-            commit;
-          exception
-            when others then
+                 values ('||PNNROJOB||',    trunc(sysdate), ''Inicio: ' || TO_CHAR(SYSDATE,'dd-mm-yyyy HH24:mi:ss')||''')';
+     EXECUTE IMMEDIATE VSSQL;
+  END IF;
+  IF VSHORA = '01' THEN
+     VSTIPOCARGA := 'Total';
+  ELSIF VSHORA IN ('09', '13')  THEN
+        VSTIPOCARGA := 'Parcial';
+  END IF;
+  IF VSHORA IN ('01', '09', '13')  THEN
+     FOR I IN (SELECT * FROM MAX_EMPRESA A
+               WHERE A.STATUS = 'A'
+               AND  MOD(A.NROEMPRESA,5) = PNMODEMPRESA
+               AND  NROEMPRESA < 500
+               ORDER BY 1)
+     LOOP
+       OBJ_PARAM_SMTP := C5_TP_PARAM_SMTP(1);
+       IF VSHORA IN ('01', '09', '13')  THEN
+          IF VSHORA <> '03' THEN
+             --GERANDO PROMOCAO
+             PKG_MAD_ADMPRECO.SP_GERAPROMOCAO(I.NROSEGMENTOPRINC, I.NROEMPRESA, TRUNC(SYSDATE), 'AUTOMATICO');
+             --VALIDANDO PRECOS
+             PKG_MAD_ADMPRECO.SP_VALIDAPRECO(I.NROSEGMENTOPRINC, I.NROEMPRESA,'AUTOMATICO', 'T');
+          END IF;
+          BEGIN
+            SPMRL_CARGAPDVCORAL(I.NROEMPRESA, TRUNC(SYSDATE), VSTIPOCARGA, NULL);
+            COMMIT;
+          EXCEPTION
+            WHEN OTHERS THEN
 
-              --- Comentado por Cipolla em 06/03/2023, caso apresente erro será enviado e-mail. Solicitação Lucimar - Ticket c5 16317714
-/*              vsSql :=
+              --- COMENTADO POR CIPOLLA EM 06/03/2023, CASO APRESENTE ERRO SERÁ ENVIADO E-MAIL. SOLICITAÇÃO LUCIMAR - TICKET C5 16317714
+/*              VSSQL :=
               'insert into nag_job(nrojob, dtamovimento,  nroempresa,  erro)
-                          values ('||pnNroJob||',    trunc(sysdate), i.nroempresa, ''Erro ao gerar a carga de PDV da empresa'')';
-              execute immediate vsSql;*/
+                          values ('||PNNROJOB||',    trunc(sysdate), i.nroempresa, ''Erro ao gerar a carga de PDV da empresa'')';
+              EXECUTE IMMEDIATE VSSQL;*/
 
-              sp_envia_email(obj_param      => obj_param_smtp,
-                             psDestinatario => 'giuliano.gomes@nagumo.com.br',
-                             psAssunto      => 'Erro ao gerar a carga de PDV da empresa ' || to_char(i.nroempresa),
-                             psMensagem     => 'Erro ao gerar a carga de PDV da empresa ' || to_char(i.nroempresa),
-                             psindusahtml   => 'N');
-          end;
-          -- Adicionado por Giuliano - Tratamento Parcial/Total Balanca
-          IF vsTipoCarga = 'Total' THEN
-          begin
-            for bal in (select nroempresa, softpdv
-                        from   mrl_empsoftpdv
-                        where  tiposoft   =  'B'
-                        and    nroempresa =  i.nroempresa)
-            loop
-              insert  into mrl_logexportacao(nroempresa,       softpdv,         dtahorexportacao,     usuexportacao,
-                                             tipolog,          param1,          param2,               param3,
-                                             param4,           param5,          dtamovimento )
-                                   values   (bal.nroempresa,   bal.softpdv,     sysdate,              'JOB209',
-                                             'T',              null,            null,                 trunc(sysdate),
-                                             null,             null,            null) ;
-              commit;
-            end loop;
-            exception
+              SP_ENVIA_EMAIL(OBJ_PARAM      => OBJ_PARAM_SMTP,
+                             PSDESTINATARIO => 'giuliano.gomes@nagumo.com.br',
+                             PSASSUNTO      => 'Erro ao gerar a carga de PDV da empresa ' || TO_CHAR(I.NROEMPRESA),
+                             PSMENSAGEM     => 'Erro ao gerar a carga de PDV da empresa ' || TO_CHAR(I.NROEMPRESA)
+                                               ||'ERRO: '||SQLERRM,
+                             PSINDUSAHTML   => 'N');
+          END;
+          -- ADICIONADO POR GIULIANO - TRATAMENTO PARCIAL/TOTAL BALANCA
+          IF VSTIPOCARGA = 'Total' THEN
+          BEGIN
+            FOR BAL IN (SELECT NROEMPRESA, SOFTPDV
+                        FROM   MRL_EMPSOFTPDV
+                        WHERE  TIPOSOFT   =  'B'
+                        AND    SOFTPDV = 'TOLEDOPRIX4' -- Adicionado filtro por Giuliano | Dif ESP_TOLEDO_PARCIAL nao entra no loop
+                        AND    NROEMPRESA =  I.NROEMPRESA)
+            LOOP
+              INSERT  INTO MRL_LOGEXPORTACAO(NROEMPRESA,       SOFTPDV,         DTAHOREXPORTACAO,     USUEXPORTACAO,
+                                             TIPOLOG,          PARAM1,          PARAM2,               PARAM3,
+                                             PARAM4,           PARAM5,          DTAMOVIMENTO )
+                                   VALUES   (BAL.NROEMPRESA,   BAL.SOFTPDV,     SYSDATE,              'JOB209',
+                                             'T',              NULL,            NULL,                 TRUNC(SYSDATE),
+                                             NULL,             NULL,            NULL) ;
+              COMMIT;
+            END LOOP;
+            EXCEPTION
 
-            when others then
+            WHEN OTHERS THEN
 
-              sp_envia_email(obj_param      => obj_param_smtp,
-                             psDestinatario => 'giuliano.gomes@nagumo.com.br',
-                             psAssunto      => 'Erro ao gerar a carga TOTAL da balança da empresa: ' || to_char(i.nroempresa),
-                             psMensagem     => 'Erro ao gerar a carga TOTAL da balança da empresa: ' || to_char(i.nroempresa),
-                             psindusahtml   => 'N');
+              SP_ENVIA_EMAIL(OBJ_PARAM      => OBJ_PARAM_SMTP,
+                             PSDESTINATARIO => 'giuliano.gomes@nagumo.com.br',
+                             PSASSUNTO      => 'Erro ao gerar a carga TOTAL da balança da empresa: ' || TO_CHAR(I.NROEMPRESA),
+                             PSMENSAGEM     => 'Erro ao gerar a carga TOTAL da balança da empresa: ' || TO_CHAR(I.NROEMPRESA)
+                                               ||'ERRO: '||SQLERRM,
+                             PSINDUSAHTML   => 'N');
             END;
-          ELSE -- Parcial
+          ELSE
             BEGIN
-              ESPP_CPT_GERACARGATOLETO(i.NROEMPRESA,SYSDATE, 'N');
+              ESPP_CPT_GERACARGATOLETO(I.NROEMPRESA,SYSDATE, 'N');
                COMMIT;                
                 
-              exception
+              EXCEPTION
 
-            when others then
+            WHEN OTHERS THEN
 
-              sp_envia_email(obj_param      => obj_param_smtp,
-                             psDestinatario => 'giuliano.gomes@nagumo.com.br',
-                             psAssunto      => 'Erro ao gerar a carga PARCIAL da balança da empresa: ' || to_char(i.NROEMPRESA),
-                             psMensagem     => 'Erro ao gerar a carga PARCIAL da balança da empresa: ' || to_char(i.nroempresa),
-                             psindusahtml   => 'N');
+              SP_ENVIA_EMAIL(OBJ_PARAM      => OBJ_PARAM_SMTP,
+                             PSDESTINATARIO => 'giuliano.gomes@nagumo.com.br',
+                             PSASSUNTO      => 'Erro ao gerar a carga PARCIAL da balança da empresa: ' || TO_CHAR(I.NROEMPRESA),
+                             PSMENSAGEM     => 'Erro ao gerar a carga PARCIAL da balança da empresa: ' || TO_CHAR(I.NROEMPRESA)
+                                               ||'ERRO: '||SQLERRM,
+                             PSINDUSAHTML   => 'N');
              END;
            --
            END IF;
 
-       end if;
-     end loop;
-  end if;
-  if vsHora in ('01', '09', '13')  then
-     vsSql :=
+       END IF;
+     END LOOP;
+  END IF;
+  IF VSHORA IN ('01', '09', '13')  THEN
+     VSSQL :=
      'insert into nag_job(nrojob, dtamovimento,    erro)
-                 values ('||pnNroJob||',    trunc(sysdate), ''Final: ' || to_char(sysdate,'dd-mm-yyyy HH24:mi:ss')||''')';
-  end if;
-  commit;
-end sp_carga_preco_nagumo_v2;
+                 values ('||PNNROJOB||',    trunc(sysdate), ''Final: ' || TO_CHAR(SYSDATE,'dd-mm-yyyy HH24:mi:ss')||''')';
+  END IF;
+  COMMIT;
+END SP_CARGA_PRECO_NAGUMO_V2;
