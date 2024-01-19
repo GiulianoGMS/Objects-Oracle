40652b0c224bde7a569c94b3ce11ac21f8f6882c
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2024-01-19 15:18:38 -0300

    Update MRLV_ETIQ_NAGUMO_VAREJO_VM.sql

--- MRLV_ETIQ_NAGUMO_VAREJO_VM.sql
+++ MRLV_ETIQ_NAGUMO_VAREJO_VM.sql
@@ -17,45 +17,37 @@ select  /*+optimizer_features_enable('11.2.0.4') */  d.marca, a."NROEMPRESA",a."
              when (nvl(h.precovalidpromoc,0) > 0) and (nvl(h.precovalidpromoc,0))  < trunc((a.precovalidnormal * a.padraoembvenda),2)
                then
        -- Alterado por Giuliano | Tratamento Preco Meu Nagumo
-       -- Se existir preco cartao no encarte, roda para buscar o preco meu nagumo
-       CASE WHEN EXISTS (SELECT 1 FROM CONSINCO.MRL_ENCARTE ME 
-                                  INNER JOIN CONSINCO.MRL_ENCARTEPRODUTOPRECO MI ON ME.SEQENCARTE = MI.SEQENCARTE
-                           WHERE NVL(MI.PRECOCARTAO,0) > 0
-                             AND MI.SEQPRODUTO = A.SEQPRODUTO 
-                             AND SYSDATE BETWEEN DTAINICIO AND DTAFIM
-                             AND NROAGRUPAMENTO = (SELECT NROSEGMENTOPRINC
-                                                     FROM CONSINCO.MAX_EMPRESA X 
-                                                    WHERE NROEMPRESA = A.NROEMPRESA))
+       CASE WHEN CONSINCO.NAGF_PRECOMN(A.CODACESSOPADRAO,A.NROEMPRESA) > 0 -- 7896232111064
+
        THEN                                   CHR(13) || CHR(10) ||
-        '^FO100,105^GB150,4,4^FS'          || CHR(13) || CHR(10) || -- Risco
+        '^FO90,115^GB150,4,4^FS'           || CHR(13) || CHR(10) || -- Risco
         '^FO050,190^BY2.4^BEN,25,Y,N^FD'||A.CODACESSOPADRAO||'^FS' || CHR(13) || CHR(10) || -- EAN
-        '^FO50,20^A0N,40,40^FD'||SUBSTR(A.DESCCOMPLETA,0,35) ||' '||CASE WHEN J.QTDEMBALAGEM > 1 THEN J.EMBALAGEM ELSE NULL END||'^FS' || CHR(13) || CHR(10) || -- Desc
-        '^FO50,160^A0N,20,20^FD'||G.NOMEREDUZIDO||'^FS' || CHR(13) || CHR(10) || -- Empresa
-        '^FO30,251^A0N,20,20^FD'||TO_CHAR(SYSDATE, 'DD/MM/YY HH24:MI')||'              Produto: '||A.SEQPRODUTO||'             Val. Prom.: '||
+        '^FO50,20^A0N,40,40^FD'||SUBSTR(A.DESCCOMPLETA,0,40) ||' '||CASE WHEN J.QTDEMBALAGEM > 1 THEN J.EMBALAGEM ELSE NULL END||'^FS' || CHR(13) || CHR(10) || -- Desc
+        '^FO30,251^A0N,20,20^FD'||TO_CHAR(SYSDATE, 'DD/MM/YY HH24:MI')||'   Produto: '||A.SEQPRODUTO||'   Val. Prom.: '||
                                 TO_CHAR(NAGF_INICIOPROMETIQUETA(A.NROEMPRESA, A.SEQPRODUTO, H.PRECOVALIDPROMOC), 'DD/MM/YY')||' a ' ||
-                                TO_CHAR(NAGF_FIMPROMETIQUETA   (A.NROEMPRESA, A.SEQPRODUTO, H.PRECOVALIDPROMOC), 'DD/MM/YY')||'^FS' || CHR(13) || CHR(10) ||
+                                TO_CHAR(NAGF_FIMPROMETIQUETA   (A.NROEMPRESA, A.SEQPRODUTO, H.PRECOVALIDPROMOC), 'DD/MM/YY')|| '   LJ: ' || G.NOMEREDUZIDO||'^FS' || CHR(13) || CHR(10) || 
         '^FO275,90^A0N,35,35^FDOFERTA^FS'  || CHR(13) || CHR(10) ||
         '^FO490,100^A0N,30,30^FD  R$  ^FS' || CHR(13) || CHR(10) ||
         '^FO495,76^A0N,20,20^FD  POR  ^FS' || CHR(13) || CHR(10) ||
-        '^FO610,73^A0N,58,58^FD'||LPAD(TRUNC(H.PRECOVALIDPROMOC), 4, ' ' ) || ',' || LPAD((H.PRECOVALIDPROMOC - TRUNC(H.PRECOVALIDPROMOC)) * 100, 2, 0)||'^FS' || CHR(13) || CHR(10) || -- Promoc
+        '^FO580,73^A0N,58,58^FD'||LPAD(TRUNC(H.PRECOVALIDPROMOC), 4, ' ' ) || ',' || LPAD((H.PRECOVALIDPROMOC - TRUNC(H.PRECOVALIDPROMOC)) * 100, 2, 0)||'^FS' || CHR(13) || CHR(10) || -- Promoc
         (CASE WHEN J.MULTEQPEMB IS NOT NULL  OR J.MULTEQPEMBALAGEM IS NOT NULL THEN
         '^FO590,128^A0N,15,15^FDNesta Embalagem '||
                                 CASE WHEN J.MULTEQPEMBALAGEM = 'LI' THEN 'L' when  J.MULTEQPEMBALAGEM = 'GR' THEN '100g' ELSE J.MULTEQPEMBALAGEM END ||' R$ ' ||
                                 DECODE(SIGN(J.PRECOGERPROMOC),+1,
                                 TRANSLATE(TO_CHAR(ROUND((J.PRECOGERPROMOC/(J.MULTEQPEMB*1000))*1000 ,2),'FM9990.00'), '.', ','),
-                                TRANSLATE(TO_CHAR(ROUND((J.PRECOGERNORMAL/(J.MULTEQPEMB*1000))*1000 ,2),'FM9990.00'), '.', ','))||'^FS' || CHR(13) || CHR(10) ||
+                                TRANSLATE(TO_CHAR(ROUND((J.PRECOGERNORMAL/(J.MULTEQPEMB*1000))*1000 ,2),'FM9990.00'), '.', ','))  END )||'^FS' || CHR(13) || CHR(10) ||
         '^FO690,85^A0N,090,40^FD  ^FS'     || CHR(13) || CHR(10) ||
-        '^FO50,85^A0N,25,25^FDR$^FS'       || CHR(13) || CHR(10) ||
-        '^FO51,115^A0N,20,20^FDDE^FS'      || CHR(13) || CHR(10) ||
-        '^FO70,82^A0N,60,60^FD'||LPAD(TRUNC(H.PRECOVALIDNORMAL), 4, ' ' ) || ',' || LPAD((H.PRECOVALIDNORMAL - TRUNC(H.PRECOVALIDNORMAL)) * 100, 2, '0') END )|| '^FS' || CHR(13) || CHR(10) || -- PRECO NORMAL
+        '^FO50,95^A0N,25,25^FDR$^FS'       || CHR(13) || CHR(10) ||
+        '^FO51,125^A0N,20,20^FDDE^FS'      || CHR(13) || CHR(10) ||
+        '^FO70,92^A0N,60,60^^FD'||LPAD(TRUNC(H.PRECOVALIDNORMAL), 4, ' ' ) || ',' || LPAD((H.PRECOVALIDNORMAL - TRUNC(H.PRECOVALIDNORMAL)) * 100, 2, '0')|| '^FS' || CHR(13) || CHR(10) || -- PRECO NORMAL
         '^FO260,60^GB230,85,1^FS'          || CHR(13) || CHR(10) ||
         '^FO260,155^GB530,85,1^FS'         || CHR(13) || CHR(10) ||
         '^FO490,195^A0N,30,30^FD  R$  ^FS' || CHR(13) || CHR(10) ||
         '^FO495,175^A0N,20,20^FD  POR  ^FS'|| CHR(13) || CHR(10) ||
         '^FO275,170^A0N,35,35^FDMeu Nagumo^FS' || CHR(13) || CHR(10) ||
         '^FO275,205^A0N,20,20^FDExclusivo ^FS' || CHR(13) || CHR(10) ||
-        '^FO610,167^A0N,58,58^FD'||LPAD(TRUNC(CONSINCO.NAGF_PRECOMN(A.CODACESSOPADRAO,A.NROEMPRESA)), 4, ' ' ) || ','|| 
-                                   LPAD((CONSINCO.NAGF_PRECOMN(A.CODACESSOPADRAO,A.NROEMPRESA) - 
+        '^FO580,167^A0N,58,58^FD'||LPAD(TRUNC(CONSINCO.NAGF_PRECOMN(A.CODACESSOPADRAO,A.NROEMPRESA)), 4, ' ' ) || ','||
+                                   LPAD((CONSINCO.NAGF_PRECOMN(A.CODACESSOPADRAO,A.NROEMPRESA) -
                                    TRUNC(CONSINCO.NAGF_PRECOMN(A.CODACESSOPADRAO,A.NROEMPRESA))) * 100, 2, 0)||'^FR^FS'|| CHR(13) || CHR(10) ||
         (CASE WHEN J.MULTEQPEMB IS NOT NULL  OR J.MULTEQPEMBALAGEM IS NOT NULL THEN
         '^FO590,221^A0N,15,15^FDNesta Embalagem '||
@@ -64,53 +56,36 @@ select  /*+optimizer_features_enable('11.2.0.4') */  d.marca, a."NROEMPRESA",a."
                                 TRANSLATE(TO_CHAR(ROUND((CONSINCO.NAGF_PRECOMN(A.CODACESSOPADRAO,A.NROEMPRESA)/(J.MULTEQPEMB*1000))*1000 ,2),'FM9990.00'), '.', ','),
                                 TRANSLATE(TO_CHAR(ROUND((CONSINCO.NAGF_PRECOMN(A.CODACESSOPADRAO,A.NROEMPRESA)/(J.MULTEQPEMB*1000))*1000 ,2),'FM9990.00'), '.', ',')) END)||'^FS'|| CHR(13) || CHR(10) ||
         '^FO490,155^GB300,86,51^FR^FS'     || CHR(13) || CHR(10) ||
-        '^FO490,60^GB300,85,50^FR^FS' 
-        
+        '^FO490,60^GB300,85,50^FR^FS'
+
        -- Final Preco Meu Nagumo
        ELSE
-             --and a.preco1 is not null and a.preco2 is not null then -- segundo e terceira condicao desligada
-         chr(13) || chr(10) || '^FO18,30^APN,90,40^FD'|| substr(a.Desccompleta,0,40) ||' '||case when j.qtdembalagem > 1 then j.embalagem else null end || '^FS' -- Descri¿¿¿¿o Maior
-       --chr(13) || chr(10) ||'^FO18,34^AQN,60,10^FD'|| substr(a.Desccompleta,0,40) || '^FS' -- Descri¿¿¿¿o Menor
-         ||chr(13) || chr(10) || '^FO38,210^BEN,30^FD'  ||  a.codacessopadrao ||'^FS' -- ean etq oferta
-         ||chr(13)|| chr(10)||'^FO560,20^A0N,80,80^FDOFERTA^FS'||'^FS'
-         ||chr(13)|| chr(10)||'^FO480,100^A0N,170,120^FD' || lpad(trunc(h.precovalidpromoc), 4, ' ' ) || ',' || lpad((h.precovalidpromoc - trunc(h.precovalidpromoc)) * 100, 2, 0)|| '^FS'
-         ||chr(13)|| chr(10)||'^FO535,010^GB300,270,140,B,3^FR^FS' --FUNDO PRETO
-        ||chr(13)|| chr(10)||'^FO18,07^AQN,20,20^FD' ||  'Val Prom.: ' ||     -- Alterado por Giuiano - 03/08/2023 - Ticket 270721
-         --(SELECT MAX(XX.DTAINICIOPROM) FROM CONSINCO.MRL_PROMOCAOITEM XX WHERE XX.SEQPRODUTO = A.SEQPRODUTO AND XX.NROEMPRESA = A.NROEMPRESA AND TRUNC(SYSDATE) BETWEEN XX.DTAINICIOPROM AND XX.DTAFIMPROM),'dd/mm/yy') || ' a ' ||to_char(
-         --(SELECT MAX(XX.DTAFIMPROM)    FROM CONSINCO.MRL_PROMOCAOITEM XX WHERE XX.SEQPRODUTO = A.SEQPRODUTO AND XX.NROEMPRESA = A.NROEMPRESA AND TRUNC(SYSDATE) BETWEEN XX.DTAINICIOPROM AND XX.DTAFIMPROM),'dd/mm/yy')||'^FS'
+                                              CHR(13) || CHR(10) || 
+        '^FO100,115^GB150,4,4^FS'          || CHR(13) || CHR(10) ||-- RISCO
+        '^FO050,170^BY2.4^BEN,25,Y,N^FD'||A.CODACESSOPADRAO||'^FS'|| CHR(13) || CHR(10) ||
+        '^FO50,20^A0N,40,40^FD'||SUBSTR(A.DESCCOMPLETA,0,40) ||' '||CASE WHEN J.QTDEMBALAGEM > 1 THEN J.EMBALAGEM ELSE NULL END||'^FS' || CHR(13) || CHR(10) ||
+        '^FO50,251^A0N,20,20^FD'||TO_CHAR(SYSDATE, 'DD/MM/YY HH24:MI')||'   Produto: '||A.SEQPRODUTO||'   Val. Prom.: '||
+                                TO_CHAR(NAGF_INICIOPROMETIQUETA(A.NROEMPRESA, A.SEQPRODUTO, H.PRECOVALIDPROMOC), 'DD/MM/YY')||' a ' ||
+                                TO_CHAR(NAGF_FIMPROMETIQUETA   (A.NROEMPRESA, A.SEQPRODUTO, H.PRECOVALIDPROMOC), 'DD/MM/YY')|| '   LJ: ' || G.NOMEREDUZIDO||'^FS' || CHR(13) || CHR(10) || 
+        '^FO275,120^A0N,80,60^FDOFERTA^FS'|| CHR(13) || CHR(10) ||
+        '^FO490,100^A0N,30,30^FD  R$  ^FS'|| CHR(13) || CHR(10) ||
+        '^FO495,76^A0N,20,20^FD  POR  ^FS'|| CHR(13) || CHR(10) ||
+        '^FO470,80^A0N,150,110^FD'||LPAD(TRUNC(H.PRECOVALIDPROMOC), 4, ' ' ) || ',' || LPAD((H.PRECOVALIDPROMOC - TRUNC(H.PRECOVALIDPROMOC)) * 100, 2, 0)||'^FS' || CHR(13) || CHR(10) || -- Promoc
+        (CASE WHEN J.MULTEQPEMB IS NOT NULL  OR J.MULTEQPEMBALAGEM IS NOT NULL THEN
+        '^FO590,215^A0N,15,15^FDNesta Embalagem '||
+                                CASE WHEN J.MULTEQPEMBALAGEM = 'LI' THEN 'L' when  J.MULTEQPEMBALAGEM = 'GR' THEN '100g' ELSE J.MULTEQPEMBALAGEM END ||' R$ ' ||
+                                DECODE(SIGN(J.PRECOGERPROMOC),+1,
+                                TRANSLATE(TO_CHAR(ROUND((J.PRECOGERPROMOC/(J.MULTEQPEMB*1000))*1000 ,2),'FM9990.00'), '.', ','),
+                                TRANSLATE(TO_CHAR(ROUND((J.PRECOGERNORMAL/(J.MULTEQPEMB*1000))*1000 ,2),'FM9990.00'), '.', ','))END )||'^FS' || CHR(13) || CHR(10) ||
+        '^FO690,85^A0N,090,40^FD  ^FS'|| CHR(13) || CHR(10) ||
+        '^FO50,95^A0N,25,25^FDR$^FS'|| CHR(13) || CHR(10) ||
+        '^FO51,125^A0N,20,20^FDDE^FS'|| CHR(13) || CHR(10) ||
+        '^FO70,92^A0N,60,60^FD'||LPAD(TRUNC(H.PRECOVALIDNORMAL), 4, ' ' ) || ',' || LPAD((H.PRECOVALIDNORMAL - TRUNC(H.PRECOVALIDNORMAL)) * 100, 2, '0')|| '^FS' || CHR(13) || CHR(10) || -- PRECO NORMAL
+        '^FO260,60^GB230,180,1^FS'|| CHR(13) || CHR(10) ||
+        '^FO490,60^GB300,180,100^FR^FS'
 
-         --Alterado por Giuliano em 29/11/2023 para respeitar o preço promocional
-         to_char(NAGF_INICIOPROMETIQUETA(A.NROEMPRESA, A.SEQPRODUTO, H.PRECOVALIDPROMOC), 'DD/MM/YY')|| ' a ' ||
-         to_char(NAGF_FIMPROMETIQUETA   (A.NROEMPRESA, A.SEQPRODUTO, H.PRECOVALIDPROMOC), 'DD/MM/YY')||'^FS'
-         || chr(13) || chr(10) || '^FO03,265^A0N,13,16^FD'||'LOJA:'|| g.nomereduzido || '^FS'
-         || chr(13) || chr(10) || '^FO155,265^A0N,13,16^FD' ||  to_char(sysdate, 'dd/mm/yy hh24:mi') || '^FS'
-         || chr(13) || chr(10) || '^FO260,265^A0N,13,16^FD'  || 'PROD:'|| a.seqproduto ||'^FS' -- COD INTERNO
-         || chr(13) || chr(10) || '^FO65,120^GB250,85,4^FR^FS' --gd preco a partir de
-        -- || chr(13) || chr(10) || '^^FO0150,88^APN,020,020^FD'|| 'A PARTIR DE' ||' ' || (A.Qtdembalagem1)  || ' ' || 'UNID'  ||'^FS' --PALAVRA A PARTIR DE
-         || chr(13) || chr(10) || '^FO080,130^APN,030,030^FD'||'R$'||'^FS' --R$ DO preco normal
-         || chr(13) || chr(10) || '^^FO110,125^APN,70,70^FD' || lpad(trunc(h.precovalidnormal), 4, ' ' ) || ',' || lpad((h.precovalidnormal - trunc(h.precovalidnormal)) * 100, 2, '0') ||'^FS' -- Preco Normal
-
-----PREÇO POR KG ou LI ou etc...
-||    (CASE WHEN
-       J.MULTEQPEMB IS NOT NULL  OR J.MULTEQPEMBALAGEM IS NOT NULL
-       THEN
-CHR(13) || CHR(10) || '^FO550,255^A0N,20,19^FD' ||/* '*PRECO PAGO POR ' || J.MULTEQPEMBALAGEM ||' R$   '*/ 'Nesta Embalagem ' || CASE WHEN J.MULTEQPEMBALAGEM = 'LI' THEN 'L'
-                                                                                                                                                                                                                                                                    when  J.MULTEQPEMBALAGEM = 'GR' THEN '100g'
-                                                                                                                                                                                                                                                                     ELSE J.MULTEQPEMBALAGEM END ||' R$ '
-      || DECODE(SIGN(J.PRECOGERPROMOC/*J.PRECOGERNORMAL*/),+1,
-       TRANSLATE(TO_CHAR(ROUND((J.PRECOGERPROMOC/(J.MULTEQPEMB*1000))*1000 ,2),'FM9990.00'), '.', ','),
-       TRANSLATE(TO_CHAR(ROUND((J.PRECOGERNORMAL/(J.MULTEQPEMB*1000))*1000 ,2),'FM9990.00'), '.', ','))
-|| CHR(13) || CHR(10) || ''
-END)||'^FR^FS'
-
-         ||chr(13)|| chr(10)||'^FO380,120^A0N,55,55^FDPOR^FS||^FS'  --POR
-         ---QR CODE Etiqueta Oferta
-         || chr(13) || chr(10) || '^FO0390,0165^BQ,2,4^FDLA,:p:' || (select distinct(r.codigo || ':vp:' || r.codigo_preco)
-                                                                        from rub.rub_produto r
-                                                                        where R.id_loja = g.nroempresa
-                                                                        AND R.codigo = a.seqproduto) || '^FS'
        END -- Final do case para meu nagumo
-       
+
 ---------------INICIO PRECO UNICO normal ou promo¿¿¿¿o for MAIOR que Normal
          when /*round (a.preco1/a.qtdembalagem1,2) = trunc((a.precoembpadrao/a.padraoembvenda),2) or*/
            (nvl(a.precovalidpromoc,0) >= trunc((a.precovalidnormal/a.padraoembvenda),2))
@@ -236,7 +211,7 @@ where a.seqproduto = b.seqproduto
   and i.seqfamilia = j.seqfamilia
   and i.seqproduto = j.seqproduto
 
-  
-  
+
+
   order by linha
 ;
