e97f2f7099b23f610c12498b948e1c23be539e6e
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2024-04-17 09:36:15 -0300

    Update MRLV_BASEETIQUETAPROD.sql

--- MRLV_BASEETIQUETAPROD.sql
+++ MRLV_BASEETIQUETAPROD.sql
@@ -4,140 +4,9 @@ CREATE OR REPLACE VIEW MRLV_BASEETIQUETAPROD AS
 
 SELECT DISTINCT * FROM (
 
-select /*+OPTIMIZER_FEATURES_ENABLE('11.2.0.4')*/  a.seqproduto,
-       a.nroempresa,
-       a.nrosegmento,
-       b.desccompleta,
-       b.descreduzida,
-       b.seqfamilia,
-       e.locentrada,
-       e.locsaida,
-       g.nrorua,
-       g.nropredio,
-       g.nrovao,
-       g.nrosala,
-       g.localtura,
-       g.loclargura,
-       g.locprofundidade,
-       d.embalagem || ' ' || d.qtdembalagem embalagem,
-       c.familia,
-       c.pesavel,
-       c.pmtdecimal,
-       c.pmtmultiplicacao,
-       a.statusvenda,
-       d.qtdembalagem,
-       MAX(A.PRECOBASENORMAL) precobasenormal,
-       MAX(A.PRECOGERNORMAL) precogernormal,
-       MAX(A.PRECOGERPROMOC) precogerpromoc,
-       MAX(A.PRECOVALIDNORMAL) precovalidnormal,
-       MAX(A.PRECOVALIDPROMOC) precovalidpromoc,
-       'N' INDEMIETIQUETA,
-       CASE 
-        WHEN TO_CHAR(SYSDATE, 'HH24:MI') <= '09:00' THEN 1
-        ELSE 0 -- Se a hora atual não for maior que 09:00, retorna 0 - Só irá emitir pela manha até as 09:00hrs
-       END qtdetiqueta,
-       e.nrogondola,
-       e.estqloja,
-       e.estqdeposito,
-       e.estqtroca,
-       e.estqalmoxarifado,
-       e.estqoutro,
-       e.qtdreservadavda,
-       e.qtdreservadareceb,
-       e.qtdreservadafixa,
-       SYSDATE dtageracaopreco,
-       SYSDATE dtavalidacaopreco,
-       max(a.dtabaseexportacao) dtabaseexportacao,
-       d.multeqpemb,
-       d.multeqpembalagem,
-       E.NRODEPARTAMENTO,
-       a.indetiquetarf,
-       max(a.dtageracaoprecoprog) dtageracaoprecoprog,
-       e.etiquetapadrao,
-       nvl(a.qtdetiqueta, e.qtdetiqueta) qtdetiquetarf
-      
-FROM MRL_PRODEMPSEG A INNER JOIN MAP_PRODUTO B ON A.SEQPRODUTO = B.SEQPRODUTO
-                      INNER JOIN MAP_FAMILIA C ON C.SEQFAMILIA = B.SEQFAMILIA
-                      INNER JOIN MAP_FAMEMBALAGEM D   ON D.SEQFAMILIA = B.SEQFAMILIA AND D.QTDEMBALAGEM = A.QTDEMBALAGEM
-                      INNER JOIN MRL_PRODUTOEMPRESA E ON E.SEQPRODUTO = A.SEQPRODUTO AND E.NROEMPRESA = A.NROEMPRESA
-                      INNER JOIN MAD_FAMSEGMENTO F    ON F.SEQFAMILIA = B.SEQFAMILIA AND F.NROSEGMENTO = A.NROSEGMENTO
-                      INNER JOIN MRL_PRODLOCAL G      ON G.SEQPRODUTO = A.SEQPRODUTO AND G.SEQLOCAL = E.LOCSAIDA AND G.NROEMPRESA = A.NROEMPRESA
-                     
-WHERE EXISTS     (SELECT 1 FROM MRL_ENCARTE X INNER JOIN MRL_ENCARTEPRODUTO XI      ON XI.SEQENCARTE = X.SEQENCARTE
-                                              INNER JOIN CONSINCO.MRL_ENCARTEEMP XE ON XE.SEQENCARTE = X.SEQENCARTE
-                                              INNER JOIN CONSINCO.MRL_ENCARTEPRODUTOPRECO PP ON PP.SEQENCARTE = X.SEQENCARTE AND PP.SEQPRODUTO = XI.SEQPRODUTO
-                                              INNER JOIN MAP_PRODUTO P              ON P.SEQPRODUTO  = XI.SEQPRODUTO
-                                              INNER JOIN MAP_PRODUTO PF             ON PF.SEQFAMILIA = P.SEQFAMILIA
-                                              INNER JOIN MAP_PRODSIMILAR S          ON S.SEQPRODUTO  = P.SEQPRODUTO
-                                              INNER JOIN MAP_PRODSIMILAR SF         ON SF.SEQSIMILARIDADE = S.SEQSIMILARIDADE
-                                                                              
-                           WHERE 1=1
-                             --AND X.SEQGRUPOPROMOC NOT IN (7,9)  -- Retira PROPZ / VALIDADE
-                             --AND NVL(PP.PRECOCARTAO,0) > 0      -- Apenas os que possuem preco meu nagumo
-                             AND X.DTAFIM = TRUNC(SYSDATE) - 1  -- Retornando as saidas do dia anterior
-                             AND XE.NROEMPRESA = A.NROEMPRESA   -- Retorna produtos da familia do produto que esta no encarte
-                             AND(SF.SEQPRODUTO = A.SEQPRODUTO   -- Retorna produtos que estao na similaridade do produto que esta no encarte
-                              OR PF.SEQPRODUTO = A.SEQPRODUTO))
+-- Select normal da aplicacao
 
-   AND d.qtdembalagem           =            decode( nvl(fc5maxparametro('EMISSAO_ETIQUETA', a.nroempresa, 'EMITE_ETIQ_PRECO_DIF'),'N'),
-                                                     'N', f.padraoembvenda, 'M', fRetQtdEmbBaseEtiqProd(b.seqproduto, e.nroempresa, f.nrosegmento)/*fMaiorEmbVendaAtiva(b.seqproduto, e.nroempresa, f.nrosegmento) - COMENTADO NO RC 66994*/,
-                                                          d.qtdembalagem)
-                                                          
-                                                          
-                                                         
-
-group  by
-       a.seqproduto,
-       a.nroempresa,
-       a.nrosegmento,
-       b.desccompleta,
-       b.descreduzida,
-       b.seqfamilia,
-       e.locentrada,
-       e.locsaida,
-       g.nrorua,
-       g.nropredio,
-       g.nrovao,
-       g.nrosala,
-       g.localtura,
-       g.loclargura,
-       g.locprofundidade,
-       d.embalagem || ' ' || d.qtdembalagem,
-       c.familia,
-       c.pesavel,
-       c.pmtdecimal,
-       c.pmtmultiplicacao,
-       a.statusvenda,
-       d.qtdembalagem,
-/*       round(a.precobasenormal / a.qtdembalagem * d.qtdembalagem, 2) ,
-       round(a.precogernormal / a.qtdembalagem * d.qtdembalagem, 2) ,
-       round(a.precogerpromoc / a.qtdembalagem * d.qtdembalagem, 2) ,
-       round(a.precovalidnormal / a.qtdembalagem * d.qtdembalagem, 2) ,
-       round(a.precovalidpromoc / a.qtdembalagem * d.qtdembalagem, 2) ,*/
-       a.indemietiqueta,
-       e.qtdetiqueta,
-       e.nrogondola,
-       e.estqloja,
-       e.estqdeposito,
-       e.estqtroca,
-       e.estqalmoxarifado,
-       e.estqoutro,
-       e.qtdreservadavda,
-       e.qtdreservadareceb,
-       e.qtdreservadafixa,
-       /*RC 51219 - RP 51582 - PAGOTTO*/
-       d.multeqpemb,
-       d.multeqpembalagem,
-       E.NRODEPARTAMENTO,
-       a.indetiquetarf,
-       e.etiquetapadrao,
-       a.qtdetiqueta
-
-UNION ALL
-
--- Por fim, select normal da aplicacao
-
-select a.seqproduto,
+select  /*+OPTIMIZER_FEATURES_ENABLE('11.2.0.4')*/ a.seqproduto,
        a.nroempresa,
        a.nrosegmento,
        b.desccompleta,
@@ -264,4 +133,137 @@ group  by
        E.NRODEPARTAMENTO,
        a.indetiquetarf,
        e.etiquetapadrao,
-       a.qtdetiqueta);
+       a.qtdetiqueta
+       
+UNION ALL
+
+select /*+OPTIMIZER_FEATURES_ENABLE('11.2.0.4')*/  a.seqproduto,
+       a.nroempresa,
+       a.nrosegmento,
+       b.desccompleta,
+       b.descreduzida,
+       b.seqfamilia,
+       e.locentrada,
+       e.locsaida,
+       g.nrorua,
+       g.nropredio,
+       g.nrovao,
+       g.nrosala,
+       g.localtura,
+       g.loclargura,
+       g.locprofundidade,
+       d.embalagem || ' ' || d.qtdembalagem embalagem,
+       c.familia,
+       c.pesavel,
+       c.pmtdecimal,
+       c.pmtmultiplicacao,
+       a.statusvenda,
+       d.qtdembalagem,
+       MAX(A.PRECOBASENORMAL) precobasenormal,
+       MAX(A.PRECOGERNORMAL) precogernormal,
+       MAX(A.PRECOGERPROMOC) precogerpromoc,
+       MAX(A.PRECOVALIDNORMAL) precovalidnormal,
+       MAX(A.PRECOVALIDPROMOC) precovalidpromoc,
+       'N' INDEMIETIQUETA,
+       CASE 
+        WHEN TO_CHAR(SYSDATE, 'HH24:MI') <= '09:00' THEN 1
+        ELSE 0 -- Se a hora atual não for maior que 09:00, retorna 0 - Só irá emitir pela manha
+       END qtdetiqueta,
+       e.nrogondola,
+       e.estqloja,
+       e.estqdeposito,
+       e.estqtroca,
+       e.estqalmoxarifado,
+       e.estqoutro,
+       e.qtdreservadavda,
+       e.qtdreservadareceb,
+       e.qtdreservadafixa,
+       SYSDATE dtageracaopreco,
+       SYSDATE dtavalidacaopreco,
+       max(a.dtabaseexportacao) dtabaseexportacao,
+       d.multeqpemb,
+       d.multeqpembalagem,
+       E.NRODEPARTAMENTO,
+       a.indetiquetarf,
+       max(a.dtageracaoprecoprog) dtageracaoprecoprog,
+       e.etiquetapadrao,
+       nvl(a.qtdetiqueta, e.qtdetiqueta) qtdetiquetarf
+      
+FROM MRL_PRODEMPSEG A INNER JOIN MAP_PRODUTO B ON A.SEQPRODUTO = B.SEQPRODUTO
+                      INNER JOIN MAP_FAMILIA C ON C.SEQFAMILIA = B.SEQFAMILIA
+                      INNER JOIN MAP_FAMEMBALAGEM D   ON D.SEQFAMILIA = B.SEQFAMILIA AND D.QTDEMBALAGEM = A.QTDEMBALAGEM
+                      INNER JOIN MRL_PRODUTOEMPRESA E ON E.SEQPRODUTO = A.SEQPRODUTO AND E.NROEMPRESA = A.NROEMPRESA
+                      INNER JOIN MAD_FAMSEGMENTO F    ON F.SEQFAMILIA = B.SEQFAMILIA AND F.NROSEGMENTO = A.NROSEGMENTO
+                      INNER JOIN MRL_PRODLOCAL G      ON G.SEQPRODUTO = A.SEQPRODUTO AND G.SEQLOCAL = E.LOCSAIDA AND G.NROEMPRESA = A.NROEMPRESA
+                     
+WHERE EXISTS     (SELECT 1 FROM MRL_ENCARTE X INNER JOIN MRL_ENCARTEPRODUTO XI      ON XI.SEQENCARTE = X.SEQENCARTE
+                                              INNER JOIN CONSINCO.MRL_ENCARTEEMP XE ON XE.SEQENCARTE = X.SEQENCARTE
+                                              INNER JOIN CONSINCO.MRL_ENCARTEPRODUTOPRECO PP ON PP.SEQENCARTE = X.SEQENCARTE AND PP.SEQPRODUTO = XI.SEQPRODUTO
+                                              INNER JOIN MAP_PRODUTO P              ON P.SEQPRODUTO  = XI.SEQPRODUTO
+                                              INNER JOIN MAP_PRODUTO PF             ON PF.SEQFAMILIA = P.SEQFAMILIA
+                                              INNER JOIN MAP_PRODSIMILAR S          ON S.SEQPRODUTO  = P.SEQPRODUTO
+                                              INNER JOIN MAP_PRODSIMILAR SF         ON SF.SEQSIMILARIDADE = S.SEQSIMILARIDADE
+                                                                              
+                           WHERE 1=1
+                             --AND X.SEQGRUPOPROMOC NOT IN (7,9)  -- Retira PROPZ / VALIDADE
+                             --AND NVL(PP.PRECOCARTAO,0) > 0      -- Apenas os que possuem preco meu nagumo
+                             AND X.DTAFIM = TRUNC(SYSDATE) - 1  -- Retornando as saidas do dia anterior
+                             AND XE.NROEMPRESA = A.NROEMPRESA   -- Retorna produtos da familia do produto que esta no encarte
+                             AND(SF.SEQPRODUTO = A.SEQPRODUTO   -- Retorna produtos que estao na similaridade do produto que esta no encarte
+                              OR PF.SEQPRODUTO = A.SEQPRODUTO))
+
+   AND d.qtdembalagem           =            decode( nvl(fc5maxparametro('EMISSAO_ETIQUETA', a.nroempresa, 'EMITE_ETIQ_PRECO_DIF'),'N'),
+                                                     'N', f.padraoembvenda, 'M', fRetQtdEmbBaseEtiqProd(b.seqproduto, e.nroempresa, f.nrosegmento)/*fMaiorEmbVendaAtiva(b.seqproduto, e.nroempresa, f.nrosegmento) - COMENTADO NO RC 66994*/,
+                                                          d.qtdembalagem)
+                                                          
+                                                          
+                                                         
+
+group  by
+       a.seqproduto,
+       a.nroempresa,
+       a.nrosegmento,
+       b.desccompleta,
+       b.descreduzida,
+       b.seqfamilia,
+       e.locentrada,
+       e.locsaida,
+       g.nrorua,
+       g.nropredio,
+       g.nrovao,
+       g.nrosala,
+       g.localtura,
+       g.loclargura,
+       g.locprofundidade,
+       d.embalagem || ' ' || d.qtdembalagem,
+       c.familia,
+       c.pesavel,
+       c.pmtdecimal,
+       c.pmtmultiplicacao,
+       a.statusvenda,
+       d.qtdembalagem,
+/*       round(a.precobasenormal / a.qtdembalagem * d.qtdembalagem, 2) ,
+       round(a.precogernormal / a.qtdembalagem * d.qtdembalagem, 2) ,
+       round(a.precogerpromoc / a.qtdembalagem * d.qtdembalagem, 2) ,
+       round(a.precovalidnormal / a.qtdembalagem * d.qtdembalagem, 2) ,
+       round(a.precovalidpromoc / a.qtdembalagem * d.qtdembalagem, 2) ,*/
+       a.indemietiqueta,
+       e.qtdetiqueta,
+       e.nrogondola,
+       e.estqloja,
+       e.estqdeposito,
+       e.estqtroca,
+       e.estqalmoxarifado,
+       e.estqoutro,
+       e.qtdreservadavda,
+       e.qtdreservadareceb,
+       e.qtdreservadafixa,
+       /*RC 51219 - RP 51582 - PAGOTTO*/
+       d.multeqpemb,
+       d.multeqpembalagem,
+       E.NRODEPARTAMENTO,
+       a.indetiquetarf,
+       e.etiquetapadrao,
+       a.qtdetiqueta
+
+);
