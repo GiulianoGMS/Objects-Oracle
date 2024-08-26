17c08f31f064eda55a1e7fcd5954390f829d82cb
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2024-08-26 14:45:57 -0300

    Update MRLV_PROMOCAOESPECIAL.sql

--- MRLV_PROMOCAOESPECIAL.sql
+++ MRLV_PROMOCAOESPECIAL.sql
@@ -24,9 +24,10 @@ SELECT A.SEQPRODUTO,
        A.NROEMPRESA,
        A.CODACESSOESPECIAL,
        A.VLRPRECOPROMOC,
-       -- Giuliano - 26/08/24
+       -- Alt. Giuliano - 26/08/24
        -- Divide sempre por 2 pois a etiqueta é dupla
        -- CEIL arredonda pra cima pois se for solicitado 11, irá impimir 6 etiquetas (resultando em 12 duplas)
+       -- Traz apenas a quantidade nao emitida
        CEIL((A.QTDESOLICITADA - A.QTDEETIQEMITIDA)/2) QTDESOLICITADA,
        A.DTAINICIO,
        A.DTAFIM,
