513e4dba86622c968fa20375afeb4072432d6355
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2025-01-02 22:31:18 -0300

    Update NAGF_BUSCAULTENT_ATIVO.sql

--- NAGF_BUSCAULTENT_ATIVO.sql
+++ NAGF_BUSCAULTENT_ATIVO.sql
@@ -1,24 +1,34 @@
 CREATE OR REPLACE FUNCTION CONSINCO.NAGF_BUSCAULTENT_ATIVO (psNroEmpresa NUMBER,
                                                             psSeqProduto NUMBER)
                                                             
-RETURN VARCHAR2 IS
- vsRetorno VARCHAR2(1);
+RETURN NUMBER IS
+ vsRetorno  NUMBER(2);
  
  BEGIN
+   
+ SELECT CASE WHEN NVL(MAX(X.DTAENTRADA),  DATE '2000-01-01') <= SYSDATE - 365 THEN 10 ELSE 78 END CGO
+  INTO vsRetorno
+  FROM OR_NFDESPESA X INNER JOIN OR_NFITENSDESPESA XI ON XI.SEQNOTA = X.SEQNOTA
+ WHERE X.NROEMPRESA  = psNroEmpresa
+   AND XI.CODPRODUTO = psSeqProduto;
+  
+ -- Se a ultima entrada no orc for ha mais de 365 dias (1 ano) entao olho se teve entrada por transferencia
  
- SELECT CASE WHEN MAX(NVL(Z.DTAHORLANCTO, SYSDATE)) > SYSDATE - 365 THEN 1 ELSE 0 END INDICADOR
+ IF vsRetorno = 10 THEN
+   
+ vsRetorno := NULL;
+   
+ SELECT CASE WHEN NVL(MAX(Z.DTAHORLANCTO), DATE '2000-01-01') <= SYSDATE - 365 THEN 10 ELSE 78 END CGO_FINAL
    INTO vsRetorno
    FROM MLF_NOTAFISCAL Z
   INNER JOIN MLF_NFITEM ZI
      ON ZI.SEQNF = Z.SEQNF
-  WHERE Z.NROEMPRESA = psNroEmpresa
+  WHERE Z.SEQPESSOA < 999
+   AND Z.NROEMPRESA = psNroEmpresa
    AND ZI.SEQPRODUTO = psSeqProduto;
-   
-   IF vsRetorno IS NULL THEN
-     vsRetorno := 0;
-     
-   END IF;
-   
+ 
+ END IF;
+ 
 RETURN vsRetorno;
 
 END;
